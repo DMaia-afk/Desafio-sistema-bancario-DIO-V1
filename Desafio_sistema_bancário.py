b220a93445c1b1f86d746c50a2df219562654cc7
Diego_Maia <diego.trab202@gmail.com>
2025-04-02 21:30:12 -0300

    Sistema bancário quase completo, falta ajustes

---
+++ Desafio_sistema_bancário.py
@@ -0,0 +1,76 @@
+
+menu = f'''
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+==>'''
+
+saldo = 0
+extrato = ""
+limite_por_saque = 500
+LIMITE_DE_SAQUE = 3
+total_de_saques = 0
+depositos = []
+saques = []
+
+
+def depositando_valor():
+    global saldo
+    global depositos
+    valor_depositado = input("""Insira o valor você deseja depositar: """)
+    depositos += [valor_depositado]
+    print("Parabéns, seu depósito foi realizado com sucesso!")
+    saldo += float(valor_depositado)
+
+def sacando_valor():
+    global saques
+    global total_de_saques
+    global saldo
+    global limite_por_saque
+    valor_sacado = input("""Insira o valor você deseja sacar: """)
+    total_de_saques += 1
+    
+    if float(valor_sacado) > saldo:
+        print("""###SAQUE NÃO EFETUADO, VERIFIQUE SEU SALDO###""")
+
+    elif float(valor_sacado) > limite_por_saque:
+        print("""###VOCÊ ULTRAPASSOU O LIMITE DISPONÍVEL PARA O SAQUE ATUAL###""")
+
+    else:
+        print("Parabéns, seu saque foi realizado com sucesso!")
+        saldo -= float(valor_sacado)
+        saques += [valor_sacado]
+
+
+
+while True:
+
+    escolha = input(menu)
+
+    if escolha == "d":
+        depositando_valor()
+        
+
+    elif escolha == "s":
+        
+        if total_de_saques >= LIMITE_DE_SAQUE:
+            print("""###VOCÊ JÁ ATINGIU O LIMITE DE SAQUES POSSÍVEIS POR HOJE###""")
+        else:
+            sacando_valor()
+            
+        
+
+    elif escolha == "e":
+        print(f"Seus depósitos: {depositos}")
+        print(f"Seus saques: {saques}")
+        print(f"Seu saldo: {saldo}")
+        
+    
+    elif escolha == "q":
+        break
+
+    else:
+        print("Por favor, escolha uma das opções acima para que possamos prosseguir com o serviço. O Banco D.M agradece sua compreensão.")
