dde62c635844c4397053e9f454d750a8252712cf
Diego_Maia <diego.trab202@gmail.com>
2025-04-09 17:52:52 -0300

    Formatação do texto para uma melhor apresentação

--- Desafio_sistema_bancário.py
+++ Desafio_sistema_bancário.py
@@ -1,8 +1,9 @@
 ## Importando bibliotecas para o projeto
 from datetime import date, datetime
+import pytz
 
 
-## Variáveis básicas
+## Menu principal
 menu = f'''
 
 [d] Depositar
@@ -11,51 +12,53 @@ menu = f'''
 [q] Sair
 
 ==>'''
+# Básico
+total_de_saques = 0 # Contagem de saque no total
+total_de_transacoes = 0 # Contagem de transações no total
+saldo = 0 # Saldo inicial
+extrato = "" # String que recebe todas movimentações
+LIMITE_POR_SAQUE = 500 # Limite por saque
+LIMITE_DE_SAQUE = 3 # Limite de saques
+LIMITE_DE_TRANSACOES_POR_DIA = 10 # Limite de transações
 
-saldo = 0
-extrato = ""
-limite_por_saque = 500
-LIMITE_DE_SAQUE = 3
-total_de_saques = 0
-total_de_transacoes = 0
-LIMITE_DE_TRANSACOES_POR_DIA = 10
-data = None
+# Data/hora
+data = None 
 data_formatada = None
+
+def get_hours():
+    hours = pytz.timezone("America/Sao_Paulo")
+    return datetime.now(hours).strftime("%d/%m/%Y %H:%M")
 
 
 
 ## Função de depósito
 def depositando_valor():
-    global total_de_transacoes
-    global saldo
-    global extrato
+    global total_de_transacoes, saldo, extrato
+    
     valor_depositado = input("""Insira o valor você deseja depositar: """)
-    extrato += f"Depósito: {float(valor_depositado):.2f} \n"
-    print("Parabéns, seu depósito foi realizado com sucesso!")
+    extrato += f"|    Depósito: {float(valor_depositado):.2f}\n"
+    print("\n---Parabéns, seu depósito foi realizado com sucesso!---")
     saldo += float(valor_depositado)
     total_de_transacoes += 1
 
 ## Função de saque
 def sacando_valor():
-    global total_de_transacoes
-    global extrato
-    global total_de_saques
-    global saldo
-    global limite_por_saque
+    global total_de_transacoes, extrato, total_de_saques, saldo, LIMITE_POR_SAQUE
+
     valor_sacado = input("""Insira o valor você deseja sacar: """)
     
     
     if float(valor_sacado) > saldo:
-        print("""###SAQUE NÃO EFETUADO, VERIFIQUE SEU SALDO###""")
+        print("""\n---SAQUE NÃO EFETUADO, VERIFIQUE SEU SALDO---""")
 
-    elif float(valor_sacado) > limite_por_saque:
-        print("""###VOCÊ ULTRAPASSOU O LIMITE DISPONÍVEL PARA O SAQUE ATUAL###""")
+    elif float(valor_sacado) > LIMITE_POR_SAQUE:
+        print("""\n---VOCÊ ULTRAPASSOU O LIMITE DISPONÍVEL PARA O SAQUE ATUAL---""")
 
     else:
-        print("Parabéns, seu saque foi realizado com sucesso!")
+        print("\n---Parabéns, seu saque foi realizado com sucesso!---")
         total_de_saques += 1
         saldo -= float(valor_sacado)
-        extrato += f"Saque: {float(valor_sacado):.2f} \n"
+        extrato += f"|    Saque: {float(valor_sacado):.2f}\n"
         total_de_transacoes += 1
 
 
@@ -66,34 +69,39 @@ while True:
 
     if escolha == "d":
         if LIMITE_DE_TRANSACOES_POR_DIA <= total_de_transacoes:
-            print("### LIMITE DE TRANSAÇÕES ATINGIDO, RETORNE NO PRÓXIMO DIA ###")
+            print("\n---LIMITE DE TRANSAÇÕES ATINGIDO, RETORNE NO PRÓXIMO DIA---")
         else:
             data = datetime.now()
             data_formatada = datetime.strftime(data, "%d/%m/%Y %H:%M")
-            extrato += f"Data/Hora: {data_formatada}\n"
+            extrato += f"|    Data/Hora: {data_formatada}\n"
             depositando_valor()
         
     elif escolha == "s":
         
         if total_de_saques >= LIMITE_DE_SAQUE:
-            print("""###VOCÊ JÁ ATINGIU O LIMITE DE SAQUES POSSÍVEIS POR HOJE###""")
+            print("""\n---VOCÊ JÁ ATINGIU O LIMITE DE SAQUES POSSÍVEIS POR HOJE---""")
 
         elif LIMITE_DE_TRANSACOES_POR_DIA <= total_de_transacoes:
-            print("### LIMITE DE TRANSAÇÕES ATINGIDO, RETORNE NO PRÓXIMO DIA ###")
+            print("\n---LIMITE DE TRANSAÇÕES ATINGIDO, RETORNE NO PRÓXIMO DIA---")
 
         else:
             data = datetime.now()
             data_formatada = datetime.strftime(data, "%d/%m/%Y %H:%M")
-            extrato += f"Data/Hora: {data_formatada}\n"
+            extrato += f"|    Data/Hora: {data_formatada}\n"
             sacando_valor()
         
     elif escolha == "e":
-        print(extrato)
+        if saldo <= 0:
+            print("\n---Por favor, nenhum valor identificado no saldo. Caso o erro se repita, contate nossa gerencia pelo suporte!---")
+
+        else:
+            print("-_-_-_-_-_-_- EXTRATO -_-_-_-_-_-")
+            print(extrato)
         
 
     elif escolha == "q":
         break
 
     else:
-        print("Por favor, escolha uma das opções acima para que possamos prosseguir com o serviço. O Banco D.M agradece sua compreensão.")
+        print("\n---Por favor, escolha uma das opções acima para que possamos prosseguir com o serviço. O Banco D.M agradece sua compreensão.---")
 
